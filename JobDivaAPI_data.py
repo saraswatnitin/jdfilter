36be823941a063bb4d44c5c9fbff283c97e3f2c1
saraswatnitin <110875666+saraswatnitin@users.noreply.github.com>
2022-08-10 02:31:19 -0400

    Update JobDivaAPI_data.py

--- JobDivaAPI_data.py
+++ JobDivaAPI_data.py
@@ -13,9 +13,12 @@ def check_password():
     def password_entered():
         """Checks whether a password entered by the user is correct."""
         if (
-            st.session_state["username"] in st.secrets["passwords"]
-            and st.session_state["password"]
-            == st.secrets["passwords"][st.session_state["username"]]
+            #st.session_state["username"] in st.secrets["passwords"]
+            #and st.session_state["password"]
+            #== st.secrets["passwords"][st.session_state["username"]]
+            
+            st.session_state["password"]
+            == st.secrets["passwords"][st.session_state["password"]]
         ):
             st.session_state["password_correct"] = True
             del st.session_state["password"]  # don't store username + password
