e4db6a44dfc39f748b729e7014fa34e934198588
saraswatnitin <110875666+saraswatnitin@users.noreply.github.com>
2022-08-10 06:55:30 -0400

    Update JobDivaAPI_data.py

--- JobDivaAPI_data.py
+++ JobDivaAPI_data.py
@@ -81,6 +81,7 @@ if check_password():
 
     #original_title = '<p style="font-family:Arial; color:Black; font-size: 30px; align:center;">Filter candidate records based on Skill</p>'
     #st.markdown(original_title, unsafe_allow_html=True)
+    quoted_skill='Java'
     st.title('Filter candidates based on Skill')
     skill = st.text_input('Enter Skill :')
     quoted_skill=str(skill)
