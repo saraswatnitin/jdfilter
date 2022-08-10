e0718a79833f8c76b07b30367d30b5d8c5a21518
saraswatnitin <110875666+saraswatnitin@users.noreply.github.com>
2022-08-10 01:46:42 -0400

    Update JobDivaAPI_data.py

--- JobDivaAPI_data.py
+++ JobDivaAPI_data.py
@@ -7,88 +7,120 @@ import requests
 #import json
 from io import BytesIO
 
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == st.secrets["password"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store password
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show input for password.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 Password incorrect")
+        return False
+    else:
+        # Password correct.
+        return True
+
+if check_password():
+    st.write("Here goes your normal Streamlit app...")
+    st.button("Click me")
+            
+    hide_streamlit_style = """
+                <style>
+                #MainMenu {visibility: hidden;}
+                footer {visibility: hidden;}
+                </style>
+                """
 
 
-auth_tokn=requests.get('https://api.jobdiva.com/api/authenticate',
-                  params={'clientid':1245,
-                          'username':'jobdiva.api@cogentinfo.com',
-                         'password':'Pass@001'})
+    auth_tokn=requests.get('https://api.jobdiva.com/api/authenticate',
+                      params={'clientid':1245,
+                              'username':'jobdiva.api@cogentinfo.com',
+                             'password':'Pass@001'})
 
 
 
-tokn=auth_tokn.text
+    tokn=auth_tokn.text
 
-csrf=requests.get('https://api.jobdiva.com/csrf')
-csrf=csrf.text[10:46]
-
-
-hdr={'Authorization':tokn,
-    'X-CSRF-TOKEN': csrf}
+    csrf=requests.get('https://api.jobdiva.com/csrf')
+    csrf=csrf.text[10:46]
 
 
-#st.title('Filter candidate records based on Skill: ')
-#Ask user to input Skill  and pass to parms
-
-#original_title = '<p style="font-family:Arial; color:Black; font-size: 30px; align:center;">Filter candidate records based on Skill</p>'
-#st.markdown(original_title, unsafe_allow_html=True)
-st.title('Filter candidates based on Skill')
-skill = st.text_input('Enter Skill :')
-quoted_skill=str(skill)
-#skill='.Net'
-print(skill)
-urlquery='https://api.jobdiva.com/api/jobdiva/searchCandidateProfile?maxreturned=10&candidateQuals={"catId": 28,"dcatNames": "'+quoted_skill+'"}'
-
-#Pull candidate detail
-#parms={
-#    'maxreturned':10
-#      ,
-#     'candidateQuals':
-#    {
-#            'catId': 28,
-#            'dcatNames': quoted_skill
-#           }
-#      }
-
-#payload = {'request':  json.dumps(parms) }
-
-#print(payload)
-
-search_cand_prfl=requests.post(urlquery,
-                              headers=hdr
-                              #params=parms,
-                              #params=payload
-
-                       )
-
-searched_cand=pd.read_json(BytesIO(search_cand_prfl.content),orient='records')
-
-#searched_cand=pd.DataFrame(pd.read_json(search_cand_prfl.text),index=[0])
-
-df1 = searched_cand.replace({r'\s+$': '', r'^\s+': ''}, regex=True).replace(r'\n',  ' ', regex=True)
-
-#df2=df1['zipcode'].astype(object)
-#df2=df1.fillna()
+    hdr={'Authorization':tokn,
+        'X-CSRF-TOKEN': csrf}
 
 
-# CSS to inject contained in a string
-hide_table_row_index = """
-            <style>
-            thead tr th:first-child {display:none}
-            tbody th {display:none}
-            </style>
-            """
+    #st.title('Filter candidate records based on Skill: ')
+    #Ask user to input Skill  and pass to parms
 
-# Inject CSS with Markdown
-st.markdown(hide_table_row_index, unsafe_allow_html=True)
+    #original_title = '<p style="font-family:Arial; color:Black; font-size: 30px; align:center;">Filter candidate records based on Skill</p>'
+    #st.markdown(original_title, unsafe_allow_html=True)
+    st.title('Filter candidates based on Skill')
+    skill = st.text_input('Enter Skill :')
+    quoted_skill=str(skill)
+    #skill='.Net'
+    print(skill)
+    urlquery='https://api.jobdiva.com/api/jobdiva/searchCandidateProfile?maxreturned=10&candidateQuals={"catId": 28,"dcatNames": "'+quoted_skill+'"}'
 
-#st.dataframe(searched_cand)
+    #Pull candidate detail
+    #parms={
+    #    'maxreturned':10
+    #      ,
+    #     'candidateQuals':
+    #    {
+    #            'catId': 28,
+    #            'dcatNames': quoted_skill
+    #           }
+    #      }
 
-st.dataframe(df1)
-#st.dataframe(df2)
+    #payload = {'request':  json.dumps(parms) }
+
+    #print(payload)
+
+    search_cand_prfl=requests.post(urlquery,
+                                  headers=hdr
+                                  #params=parms,
+                                  #params=payload
+
+                           )
+
+    searched_cand=pd.read_json(BytesIO(search_cand_prfl.content),orient='records')
+
+    #searched_cand=pd.DataFrame(pd.read_json(search_cand_prfl.text),index=[0])
+
+    df1 = searched_cand.replace({r'\s+$': '', r'^\s+': ''}, regex=True).replace(r'\n',  ' ', regex=True)
+
+    #df2=df1['zipcode'].astype(object)
+    #df2=df1.fillna()
+
+
+    # CSS to inject contained in a string
+    hide_table_row_index = """
+                <style>
+                thead tr th:first-child {display:none}
+                tbody th {display:none}
+                </style>
+                """
+
+    # Inject CSS with Markdown
+    st.markdown(hide_table_row_index, unsafe_allow_html=True)
+
+    #st.dataframe(searched_cand)
+
+    st.dataframe(df1)
+    #st.dataframe(df2)
 
